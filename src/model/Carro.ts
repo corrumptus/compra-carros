87e6513233834627a2f8e36b7da166c813c0adde
corrumptus <lazarinilucas42@gmail.com>
2024-05-21 19:39:07 -0300

    feat: creating the base model Carro

---
+++ src/model/Carro.ts
@@ -0,0 +1,12 @@
+type Carro = {
+    id: number,
+    numeroSerie: number,
+    modelo: string,
+    fabricante: string,
+    ano: number,
+    potencia: number,
+    preco: number,
+    estoque: number
+}
+
+export default Carro;
