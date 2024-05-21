d3be6e324d39c3d3d72c05f0ecdff923bced5587
corrumptus <lazarinilucas42@gmail.com>
2024-05-21 19:42:20 -0300

    feat: creating the base layout of the home page

--- src/pages/listar/ComprarCarros.tsx
+++ src/pages/listar/ComprarCarros.tsx
@@ -3,7 +3,24 @@ import "./comprar-carros.css";
 export default function ComprarCarros() {
   return (
     <main>
-      ComprarCarros
+      <div>
+        <h1>Lista de Carro</h1>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <td>Modelo</td>
+                <td>Fabricante</td>
+                <td>Ano</td>
+                <td>Potência</td>
+                <td>Preço</td>
+                <td>Estoque</td>
+              </tr>
+            </thead>
+            <tbody></tbody>
+          </table>
+        </div>
+      </div>
     </main>
   )
 }
