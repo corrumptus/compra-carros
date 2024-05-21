1e6aab4a5fbe40dfbc8aad471fe79bc555518cbd
corrumptus <lazarinilucas42@gmail.com>
2024-05-21 19:19:10 -0300

    feat: using the react-router to create routes and creating the root route to the home page

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ComprarCarros from './pages/listar/ComprarCarros'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ComprarCarros />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
 )
