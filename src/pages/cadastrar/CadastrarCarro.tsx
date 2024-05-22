3faaccac7c774088d57b9d88916b7d6b58f83a06
corrumptus <lazarinilucas42@gmail.com>
2024-05-22 01:52:48 -0300

    feat: creating the layout of the CadastrarCarro

--- src/pages/cadastrar/CadastrarCarro.tsx
+++ src/pages/cadastrar/CadastrarCarro.tsx
@@ -1,5 +1,37 @@
 export default function CadastrarCarro() {
   return (
-    <div>CadastrarCarro</div>
+    <main>
+      <form>
+        <div>
+          <label htmlFor="modelo">Modelo</label>
+          <input type="text" id="modelo" />
+        </div>
+        <div>
+          <label htmlFor="fabricante">Fabricante</label>
+          <input type="text" id="fabricante" />
+        </div>
+        <div>
+          <label htmlFor="n-serie">Número de Série</label>
+          <input type="number" id="n-serie" />
+        </div>
+        <div>
+          <label htmlFor="ano">Ano</label>
+          <input type="number" id="ano" />
+        </div>
+        <div>
+          <label htmlFor="potencia">Potência</label>
+          <input type="number" id="potencia" />
+        </div>
+        <div>
+          <label htmlFor="preco">Preço</label>
+          <input type="number" id="preco" />
+        </div>
+        <div>
+          <button>Voltar</button>
+          <button type="reset">Limpar</button>
+          <button>Cadastrar</button>
+        </div>
+      </form>
+    </main>
   )
 }
