699b73c6fea7792bb47e18d4343cad274a57ffb7
corrumptus <lazarinilucas42@gmail.com>
2024-05-21 23:29:32 -0300

    feat: creating the layout of the CarroComponent

--- src/pages/listar/CarroComponent.tsx
+++ src/pages/listar/CarroComponent.tsx
@@ -2,6 +2,13 @@ import Carro from "../../model/Carro";
 
 export default function CarroComponent({ carro }: { carro: Carro }) {
   return (
-    <div>CarroComponent</div>
+    <tr>
+      <td>{carro.modelo}</td>
+      <td>{carro.fabricante}</td>
+      <td>{carro.ano}</td>
+      <td>{carro.potencia}</td>
+      <td>{carro.preco}</td>
+      <td>{carro.estoque}</td>
+    </tr>
   )
 }
