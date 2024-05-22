360e835e1e09594a5af6788a5668962014ced418
corrumptus <lazarinilucas42@gmail.com>
2024-05-21 22:54:35 -0300

    feat: creating a implementation of the api for Carro with local storage

---
+++ src/api/CarrosAPILS.ts
@@ -0,0 +1,23 @@
+import Carro from "../model/Carro";
+
+class CarrosAPILS implements API<number, "id", Carro> {
+    getAll(): Carro[] {
+        throw new Error("Method not implemented.");
+    }
+
+    get(key: number): Carro | undefined {
+        throw new Error("Method not implemented.");
+    }
+
+    create(create: Omit<Carro, "id">): Carro | undefined {
+        throw new Error("Method not implemented.");
+    }
+
+    update(update: Carro): Carro | undefined {
+        throw new Error("Method not implemented.");
+    }
+
+    delete(key: number): boolean {
+        throw new Error("Method not implemented.");
+    }
+}
