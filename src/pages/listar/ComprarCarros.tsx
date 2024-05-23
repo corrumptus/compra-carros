419a4572f080cffba5019f4ab2787f93c013c3a8
corrumptus <lazarinilucas42@gmail.com>
2024-05-23 02:00:35 -0300

    feat: implementing the handleExcluir CarroComponent needed prop

--- src/pages/listar/ComprarCarros.tsx
+++ src/pages/listar/ComprarCarros.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import CarrosAPILS from "../../api/CarrosAPILS";
 import Carro from "../../model/Carro";
 import CarroComponent from "./CarroComponent";
@@ -6,7 +7,13 @@ import "./comprar-carros.css";
 export default function ComprarCarros() {
   const carrosAPI: API<number, "id", Carro> = new CarrosAPILS();
 
-  const carros = carrosAPI.getAll();
+  const [ carros, setCarros ] = useState(carrosAPI.getAll());
+
+  function handleExcluir(id: number) {
+    carrosAPI.delete(id);
+
+    setCarros(carrosAPI.getAll());
+  }
 
   return (
     <main id="listar">
@@ -24,7 +31,15 @@ export default function ComprarCarros() {
                 <th>botões</th>
               </tr>
             </thead>
-            <tbody>{carros.map(carro => <CarroComponent key={carro.id} carro={carro} />)}</tbody>
+            <tbody>{
+              carros.map(carro =>
+                <CarroComponent
+                  key={carro.id}
+                  carro={carro}
+                  handleExcluir={handleExcluir}
+                />
+              )}
+            </tbody>
           </table>
         </div>
       </div>
