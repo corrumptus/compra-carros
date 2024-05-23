392671395fd6455815fd48ee35ba1d0f4b8924e6
corrumptus <lazarinilucas42@gmail.com>
2024-05-23 01:27:53 -0300

    feat: getting the car by the route params

--- src/pages/ver/VerCarro.tsx
+++ src/pages/ver/VerCarro.tsx
@@ -1,27 +1,39 @@
+import { useNavigate, useParams } from "react-router-dom";
+import Carro from "../../model/Carro";
+import CarrosAPILS from "../../api/CarrosAPILS";
+
 export default function VerCarro() {
+  const navigator = useNavigate();
+
+  const { id } = useParams();
+
+  const carrosAPI: API<number, "id", Carro> = new CarrosAPILS();
+
+  const carro = carrosAPI.get(Number(id));
+
   return (
     <main id="ver">
       <div className="container">
-        <h1></h1>
+        <h1>{carro.modelo}</h1>
         <div>
           <p>Fabricante</p>
-          <span></span>
+          <span>{carro.fabricante}</span>
         </div>
         <div>
           <p>Ano</p>
-          <span></span>
+          <span>{carro.ano}</span>
         </div>
         <div>
           <p>Potência</p>
-          <span></span>
+          <span>{carro.potencia}</span>
         </div>
         <div>
           <p>Preço</p>
-          <span></span>
+          <span>{carro.preco}</span>
         </div>
         <div>
           <p>Número de série</p>
-          <span></span>
+          <span>{carro.numeroSerie}</span>
         </div>
         <div>
           <button>Voltar</button>
