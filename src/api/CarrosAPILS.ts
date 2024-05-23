5e56a357ac17db50a35826f5374bf0a49a5c703c
corrumptus <lazarinilucas42@gmail.com>
2024-05-23 18:19:00 -0300

    feat: changing the error messages

--- src/api/CarrosAPILS.ts
+++ src/api/CarrosAPILS.ts
@@ -63,39 +63,39 @@ export default class CarrosAPILS implements API<number, "id", Carro> {
 
     private verificaCarro(carro: Partial<Carro>, carros: Carro[], isEditing: boolean) {
         if (!isEditing && carro.id !== undefined)
-            throw new Error("Carro id must be undefined");
+            throw new Error("Id must be undefined");
 
         if (isEditing && carro.id === undefined)
-            throw new Error("Carro id is missing");
+            throw new Error("Id is missing");
 
         if (carro.modelo === undefined || !this.isString(carro.modelo))
-            throw new Error("Carro modelo must be a valid string");
+            throw new Error("Modelo must be a valid string");
 
         if (carro.fabricante === undefined || !this.isString(carro.fabricante))
-            throw new Error("Carro fabricante must be a valid string");
+            throw new Error("Fabricante must be a valid string");
 
         if (carro.ano === undefined || !this.isNumber(carro.ano))
-            throw new Error("Carro ano must be a valid number");
+            throw new Error("Ano must be a valid number");
 
         if (carro.potencia === undefined || !this.isNumber(carro.potencia))
-            throw new Error("Carro potencia must be a valid number");
+            throw new Error("Potencia must be a valid number");
 
         if (carro.preco === undefined || !this.isNumber(carro.preco))
-            throw new Error("Carro preco must be a valid number");
+            throw new Error("Preco must be a valid number");
 
         if (carro.numeroSerie === undefined || !this.isNumber(carro.numeroSerie))
-            throw new Error("Carro numero de serie must be a valid number");
+            throw new Error("Numero de serie must be a valid number");
 
         if (
             carros.find(c => c.id !== carro.id && c.numeroSerie === carro.numeroSerie) !== undefined
         )
-            throw new Error(`Carro with the numero de serie ${carro.numeroSerie} already exists`);
+            throw new Error(`Car with the numero de serie ${carro.numeroSerie} already exists`);
 
         if (!isEditing && carros.find(c => c.numeroSerie === carro.numeroSerie) !== undefined)
             throw new Error("Numero de serie already exists");
 
         if (isEditing && carros.find(c => c.id === carro.id) === undefined)
-            throw new Error(`Carro with the id ${carro.id} not exists`);
+            throw new Error(`Car with the id ${carro.id} not exists`);
     }
 
     private isString(value: string): value is string {
