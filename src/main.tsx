2cf78677d731525c48ff9a41cde06cd9cf2b57eb
corrumptus <lazarinilucas42@gmail.com>
2024-05-22 23:21:18 -0300

    feat: creating the atualizar route

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ActionBar from './ActionBar'
+import AtualizarCarro from './pages/atualizar/AtualizarCarro'
 import ComprarCarros from './pages/listar/ComprarCarros'
 import CadastrarCarro from './pages/cadastrar/CadastrarCarro'
 import "./index.css"
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<ComprarCarros />} />
         <Route path="/carro" element={<CadastrarCarro />} />
+        <Route path="/carro/:id" element={<AtualizarCarro />} />
       </Routes>
     </Router>
   </React.StrictMode>
