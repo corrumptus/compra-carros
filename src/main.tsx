cf573869a4ca4a4d52d79d8d8f92d0c3f1e3e09a
corrumptus <lazarinilucas42@gmail.com>
2024-05-22 23:01:43 -0300

    feat: adding a action bar into the site

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ComprarCarros from './pages/listar/ComprarCarros'
 import CadastrarCarro from './pages/cadastrar/CadastrarCarro'
 import "./index.css"
+import ActionBar from './ActionBar'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
+      <ActionBar />
       <Routes>
         <Route path="/" element={<ComprarCarros />} />
         <Route path="/carro" element={<CadastrarCarro />} />
