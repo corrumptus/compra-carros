e9c7c6230d69f77457a45d5fd721c1f11cb0f215
corrumptus <lazarinilucas42@gmail.com>
2024-05-23 18:19:55 -0300

    feat: implementing a toaster api

---
+++ src/toaster/Toaster.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export default function useToasters() {
+  const [ messages, setMessages ] = useState<string[]>([]);
+  
+  function add(message: string) {
+    setMessages(prev => [ ...prev, message ]);
+  }
+
+  return [ <Toasters messages={messages} />, add ] as const;
+}
+
+function Toasters({ messages }: { messages: string[] }) {
+  return (
+    <div id="toaster-container">
+      {messages.map(m => <Toaster error={m} key={m} />)}
+    </div>
+  )
+}
+
+function Toaster({ error }: { error: string }) {
+  return (
+    <div id="toaster">
+      {error}
+    </div>
+  )
+}
