6f15752e79eca8c9f153ba59eb52d9ba88e2eac9
corrumptus <lazarinilucas42@gmail.com>
2024-05-23 01:26:08 -0300

    feat: creating the ver page layout

---
+++ src/pages/ver/VerCarro.tsx
@@ -0,0 +1,34 @@
+export default function VerCarro() {
+  return (
+    <main id="ver">
+      <div className="container">
+        <h1></h1>
+        <div>
+          <p>Fabricante</p>
+          <span></span>
+        </div>
+        <div>
+          <p>Ano</p>
+          <span></span>
+        </div>
+        <div>
+          <p>Potência</p>
+          <span></span>
+        </div>
+        <div>
+          <p>Preço</p>
+          <span></span>
+        </div>
+        <div>
+          <p>Número de série</p>
+          <span></span>
+        </div>
+        <div>
+          <button>Voltar</button>
+          <button>Excluir</button>
+          <button>Editar</button>
+        </div>
+      </div>
+    </main>
+  )
+}
