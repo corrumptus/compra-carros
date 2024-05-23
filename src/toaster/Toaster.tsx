ce51bc9bb93cada0672ac2e56d3c4b5b683e88ee
corrumptus <lazarinilucas42@gmail.com>
2024-05-23 19:09:20 -0300

    feat: adding timeout to the toaster

--- src/toaster/Toaster.tsx
+++ src/toaster/Toaster.tsx
@@ -3,25 +3,65 @@ import "./toaster.css";
 
 export default function useToasters() {
   const [ messages, setMessages ] = useState<string[]>([]);
-  
+  const [ timeouts, setTimeouts ] = useState<number[]>([]);
+
   function add(message: string) {
     setMessages(prev => [ ...prev, message ]);
+
+    const timeout = setTimeout(() => {
+      deleteToaster(timeout);
+    }, 3000);
+
+    setTimeouts(prev => [ ...prev, timeout ]);
   }
 
-  return [ <Toasters messages={messages} />, add ] as const;
+  function deleteToaster(timeout: number) {
+    clearTimeout(timeout);
+
+    setTimeouts(prev => prev.filter(t => t === timeout));
+  }
+
+  return [
+    <Toasters
+      messages={messages}
+      timeouts={timeouts}
+      deleteToaster={deleteToaster}
+    />,
+    add
+  ] as const;
 }
 
-function Toasters({ messages }: { messages: string[] }) {
+function Toasters({
+  messages,
+  timeouts,
+  deleteToaster
+}: {
+  messages: string[],
+  timeouts: number[],
+  deleteToaster: (timeout: number) => void
+}) {
   return (
     <div className="toaster-container">
-      {messages.map((m, i) => <Toaster error={m} key={m+i} />)}
+      {messages.map((m, i) =>
+        <Toaster
+          key={timeouts[i]}
+          error={m}
+          deleteToaster={() => deleteToaster(timeouts[i])}
+        />
+      )}
     </div>
   )
 }
 
-function Toaster({ error }: { error: string }) {
+function Toaster({
+  error,
+  deleteToaster
+}: {
+  error: string,
+  deleteToaster: () => void
+}) {
   return (
-    <div className="toaster">
+    <div className="toaster" onClick={deleteToaster}>
       {error}
     </div>
   )
